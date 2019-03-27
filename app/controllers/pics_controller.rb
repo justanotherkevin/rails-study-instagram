a16c4d2c6231d80c3f2f9766730315e555179d69
Kevin Hu <hu.kevin.w@gmail.com>
2019-03-27 15:25:58 -0400

    Add routes for create, destroy on pic controller

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -1,15 +1,43 @@
 class PicsController < ApplicationController
+    before_action :get_pic, only: [:show, :edit, :update, :destroy]
+
     def index
+        @pics = Pic.all.order("created_at DESC")
     end 
+    def show 
+        @pic = Pic.find(params[:id])
+    end
     def new 
         @pic = Pic.new
     end
     def create 
         @pic = Pic.new(pic_params)
+        if @pic.save 
+            flash[:notice] = "Picture was posted"
+            redirect_to @pic
+        else 
+            render 'new'
+        end
     end
-
+    def edit 
+    end
+    def update 
+        if @pic.save 
+            flash[:notice] = "Picture was updated"
+            redirect_to @pic
+        else 
+            render 'edit'
+        end
+    end
+    def destroy
+        @pic.destroy
+        redirect_to root_path
+    end
     private
     def pic_params
         params.require(:pic).permit(:title, :description)
+    end
+    def get_pic
+        @pic = Pic.find(params[:id])
     end
 end
